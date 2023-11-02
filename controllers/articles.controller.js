d81b035b9c0607a5333caa22087aa5ac47738f27
reshad21 <miakala7@gmail.com>
2023-11-02 23:23:25 +0600

    backend database collection name change

--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -1,16 +1,21 @@
 const { getDb } = require("../utils/dbconnect")
 
 module.exports.getAllArticls = async (req, res) => {
-    const db = getDb();
-    const result = await db.collection("articls").find({}).toArray();
-    res.send(result);
+    try {
+        const db = getDb();
+        const result = await db.collection("blog").find({}).toArray();
+        // res.send(result);
+        res.status(200).json({ success: true, data: result });
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 module.exports.saveArticle = async (req, res, next) => {
     try {
         const db = getDb();
         const item = req.body;
-        const result = await db.collection("articls").insertOne(item);
+        const result = await db.collection("blog").insertOne(item);
         res.status(200).json({ success: true, data: result });
     } catch (error) {
         next(error);
